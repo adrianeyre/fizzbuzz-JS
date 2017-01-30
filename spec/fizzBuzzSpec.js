40ef7d757fcb9f2bfeda0828b1fd7b02458cf59c
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-30 12:47:36 +0000

    Better spec descriptions

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,17 +1,17 @@
 describe("fizzBuzz", function() {
-  it("result to equal Fizz", function() {
+  it("fizzBuzz(3) result to equal Fizz", function() {
     expect(fizzBuzz(3)).toEqual('Fizz');
   });
 
-  it("result to equal Buzz", function() {
+  it("fizzBuzz(5) result to equal Buzz", function() {
     expect(fizzBuzz(5)).toEqual('Buzz');
   });
 
-  it("result to equal FizzBuzz", function() {
+  it("fizzBuzz(15) result to equal FizzBuzz", function() {
     expect(fizzBuzz(15)).toEqual('FizzBuzz');
   });
 
-  it("result to equal 7", function() {
+  it("fizzBuzz(7) result to equal 7", function() {
     expect(fizzBuzz(7)).toEqual(7);
   });
 });
