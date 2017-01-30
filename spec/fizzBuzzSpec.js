712852e70a34558c513e3e4e1adf241633363e28
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-30 13:08:00 +0000

    Added a different way for FizzBuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -15,3 +15,53 @@ describe("fizzBuzz", function() {
     expect(fizzBuzz(7)).toEqual(7);
   });
 });
+
+
+describe('FizzBuzz2', function() {
+
+  var fizzBuzz2;
+
+  beforeEach(function() {
+    fizzBuzz2 = new FizzBuzz2();
+  });
+
+  describe('multiples of 3', function() {
+    it('fizzes for 3', function() {
+      expect(fizzBuzz2.play(3)).toEqual('Fizz');
+    });
+
+    it('fizzes for 6', function() {
+      expect(fizzBuzz2.play(6)).toEqual('Fizz');
+    });
+  });
+
+  describe('multiples of 5', function() {
+    it('buzzes for 5', function() {
+      expect(fizzBuzz2.play(5)).toEqual('Buzz');
+    });
+
+    it('buzzes for 10', function() {
+      expect(fizzBuzz2.play(10)).toEqual('Buzz');
+    });
+  });
+
+  describe('multiples of 3 and 5', function() {
+    it('fizzbuzzes for 15', function() {
+      expect(fizzBuzz2.play(15)).toEqual('FizzBuzz');
+    });
+
+    it('fizzbuzzes for 30', function() {
+      expect(fizzBuzz2.play(30)).toEqual('FizzBuzz');
+    });
+  });
+
+  describe('all other numbers', function() {
+    it('1 for 1', function() {
+      expect(fizzBuzz2.play(1)).toEqual(1);
+    });
+
+    it('11 for 11', function() {
+      expect(fizzBuzz2.play(11)).toEqual(11);
+    });
+  });
+});
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -4,3 +4,24 @@ function fizzBuzz(number) {
   if (number % 5 == 0) {result += 'Buzz'};
   if (result == "") {return number} else {return result} ;
 }
+
+
+
+
+function FizzBuzz2() {}
+
+FizzBuzz2.prototype.play = function(number) {
+  if (this._isDivisibleBy(15, number)) {
+    return 'FizzBuzz';
+  } else if (this._isDivisibleBy(5, number)) {
+    return 'Buzz';
+  } else if (this._isDivisibleBy(3, number)) {
+    return 'Fizz';
+  } else {
+    return number;
+  }
+}
+
+FizzBuzz2.prototype._isDivisibleBy = function(divisor, number) {
+  return number % divisor === 0;
+}
