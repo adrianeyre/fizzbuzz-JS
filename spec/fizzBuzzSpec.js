66d27e8f6aa5874f26c9568ea4ef10b365a76102
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-30 12:46:07 +0000

    Now returns number if not Fizz or Buzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -10,4 +10,8 @@ describe("fizzBuzz", function() {
   it("result to equal FizzBuzz", function() {
     expect(fizzBuzz(15)).toEqual('FizzBuzz');
   });
+
+  it("result to equal 7", function() {
+    expect(fizzBuzz(7)).toEqual(7);
+  });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,5 +2,5 @@ function fizzBuzz(number) {
   var result = "";
   if (number % 3 == 0) {result += 'Fizz'};
   if (number % 5 == 0) {result += 'Buzz'};
-  return result;
+  if (result == "") {return number} else {return result} ;
 }
